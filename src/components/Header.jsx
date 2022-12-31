75b97aebd6d17bf7dbc675ff33796071edd0ee26
Ygor Goulart <ygorgm95@gmail.com>
2022-12-31 19:51:59 -0300

    add social links and version tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,17 @@ import logo from "../images/ui/Logo.svg";
 export const Header = () => {
   return (
     <header className="header">
+      <div className="social-links">
+        <a href="https://github.com/ygg-m/fg-input-translator">
+          <i class="fa-brands fa-github"></i>
+        </a>
+        <a href="https://discord.gg/ZapfK82Fjk">
+          <i class="fa-brands fa-discord"></i>
+        </a>
+      </div>
       <Logo color="#444" />
       <h1>Fight Game Input Translator</h1>
-      <span>alpha</span>
+
       <h3>
         Translate <i>numpad notations</i> into <i>visual inputs</i>!
       </h3>
