1d7a65727261eb3e3462ed297d2695fcd5ce8cfb
Ygor Goulart <ygorgm95@gmail.com>
2023-01-04 08:28:20 -0300

    fix remove anoying minor error from console.log

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -4,28 +4,29 @@ export const Tooltip = ({ elRef, obj, visible, offset }) => {
   const haveKnowMore = moreLink?.length > 0;
   const haveKnowMoreName = moreName?.length > 0;
 
-  return (
-    <div
-      className={
-        visible ? "tooltip-container show" : "tooltip-container hidden"
-      }
-      style={{
-        bottom: elRef?.current?.offsetTop + offset,
-        left: elRef?.current?.offsetLeft + elRef?.current?.offsetWidth / 2,
-      }}
-    >
-      <div className="tooltip">
-        <span>{name}</span>
-        <div className="tooltip-input">
-          Input: <span>{input}</span>
+  if (elRef?.current?.offsetTop)
+    return (
+      <div
+        className={
+          visible ? "tooltip-container show" : "tooltip-container hidden"
+        }
+        style={{
+          bottom: elRef?.current?.offsetTop + offset,
+          left: elRef?.current?.offsetLeft + elRef?.current?.offsetWidth / 2,
+        }}
+      >
+        <div className="tooltip">
+          <span>{name}</span>
+          <div className="tooltip-input">
+            Input: <span>{input}</span>
+          </div>
+          {haveDescription && <span>{description}</span>}
+          {haveKnowMore && (
+            <a className="more-link" href={moreLink}>
+              {haveKnowMoreName ? `${moreName} →` : "Know More →"}
+            </a>
+          )}
         </div>
-        {haveDescription && <span>{description}</span>}
-        {haveKnowMore && (
-          <a className="more-link" href={moreLink}>
-            {haveKnowMoreName ? `${moreName} →` : "Know More →"}
-          </a>
-        )}
       </div>
-    </div>
-  );
+    );
 };
