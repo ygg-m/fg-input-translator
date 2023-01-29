530c682c0cadacd5cd4dea1d1831f66439c639ab
Ygor Goulart <ygorgm95@gmail.com>
2023-01-29 18:15:19 -0300

    style better looking input inside tooltip

--- src/components/TooltipContent.jsx
+++ src/components/TooltipContent.jsx
@@ -1,14 +1,24 @@
+import { v4 as uuidv4 } from "uuid";
 export const TooltipContent = ({ obj }) => {
   const { name, description, input, moreLink, moreName } = obj;
   const haveDescription = description?.length > 0;
   const haveKnowMore = moreLink?.length > 0;
   const haveKnowMoreName = moreName?.length > 0;
   const isInputArray = Array.isArray(input);
+  let inputList;
+  if (isInputArray) {
+    inputList = input
+      .map((e) => <span key={uuidv4()}>{e}</span>)
+      .map((e, i) => {
+        return [e, i !== input.length - 1 ? " or " : ""];
+      });
+  }
+
   return (
     <>
       <span>{name}</span>
       <div className="tooltip-input">
-        Input: <span>{isInputArray ? input.join(", ") : input}</span>
+        Input: {isInputArray ? inputList : <span>{input}</span>}
       </div>
       {haveDescription && <span>{description}</span>}
       {haveKnowMore && (
