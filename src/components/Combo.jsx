385301b13b9f7559a6c6577db07b6331bea2c974
Ygor Goulart <ygorgm95@gmail.com>
2023-01-30 00:09:55 -0300

    style inputs transferred to tailwind

--- src/components/Combo.jsx
+++ src/components/Combo.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Combo = ({ children }) => {
-  return <div className="flex items-center justify-center p-2">{children}</div>;
+  return (
+    <div className="flex w-fit items-center justify-center gap-1 p-2 rounded-lg bg-neutral-800">
+      {children}
+    </div>
+  );
 };
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,19 +2,21 @@ import { Tooltip, TooltipWrapper } from "react-tooltip";
 import { TooltipContent } from "./index";
 
 export const Input = ({ inputObj, tech }) => {
-  const { name } = inputObj;
+  const { name, type } = inputObj;
   const haveImg = inputObj.img !== "" || inputObj.img?.length > 0;
   const haveStyle = typeof inputObj.style === "object";
 
   const Container = ({ children }) => {
-    return <div className="w-4 h-4">{children}</div>;
+    return <div className="min-w-[2rem]">{children}</div>;
   };
 
   if (!haveImg && !haveStyle) {
     return (
       <Container>
         <TooltipWrapper tooltipId={name}>
-          <div className="w-full">{name}</div>
+          <div className="w-fit px-2 rounded-full bg-neutral-900 select-none duration-200 hover:text-cyan-500">
+            {name}
+          </div>
           {tech && tech}
         </TooltipWrapper>
         <Tooltip
@@ -31,7 +33,7 @@ export const Input = ({ inputObj, tech }) => {
     return (
       <Container>
         <TooltipWrapper tooltipId={name}>
-          {img}
+          <div className="duration-200 hover:shadow-cyan">{img}</div>
           {tech && tech}
         </TooltipWrapper>
         <Tooltip
@@ -48,7 +50,9 @@ export const Input = ({ inputObj, tech }) => {
     return (
       <Container>
         <TooltipWrapper tooltipId={name}>
-          <div style={style}>{img}</div>
+          <div style={style} className="duration-200 hover:shadow-cyan">
+            {img}
+          </div>
           {tech && tech}
         </TooltipWrapper>
         <Tooltip
