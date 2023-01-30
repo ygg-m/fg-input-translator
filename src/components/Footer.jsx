c3959bd442283323061a65670247f46788b6312f
Ygor Goulart <ygorgm95@gmail.com>
2023-01-29 23:12:17 -0300

    add padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="flex justify-center w-full">
+    <div className="py-8 flex justify-center w-full">
       Made by{" "}
       <a
         className="ml-2 font-bold underline decoration-neutral-700 hover:no-underline duration-200 border border-transparent rounded-lg pb-1 hover:px-3 hover:border-cyan-500  outline-cyan-500 hover:text-cyan-500 hover:shadow-cyan"
