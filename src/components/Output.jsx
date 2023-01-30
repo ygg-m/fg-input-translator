d2432ab02ebdc67b0fb4c1bd008a325fe4b70a70
Ygor Goulart <ygorgm95@gmail.com>
2023-01-30 00:43:40 -0300

    style vertical gap

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -3,5 +3,9 @@ import { useInput } from "../contexts/InputContext";
 
 export const Output = () => {
   const { output } = useInput();
-  return <div className="flex items-center justify-center flex-wrap p-2 outline outline-1 outline-neutral-800 rounded-lg">{output}</div>;
+  return (
+    <div className="flex items-center justify-center flex-wrap gap-y-2 p-2 outline outline-1 outline-neutral-800 rounded-lg">
+      {output}
+    </div>
+  );
 };
