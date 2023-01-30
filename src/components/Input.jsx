6377235247da5456f72a674daff0be4bc5dd1019
Ygor Goulart <ygorgm95@gmail.com>
2023-01-29 23:05:48 -0300

    style remove unnecessary classses

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,7 @@ export const Input = () => {
 
   return (
     <input
-      className="py-2 px-4 z-10 w-full rounded-2xl border-4 border-neutral-800 outline-1 outline-neutral-700 bg-neutral-700 text-neutral-500"
+      className="py-2 px-4 z-10 w-full duration-200 rounded-xl border-4 border-neutral-900 outline outline-1 outline-neutral-800 bg-neutral-800 text-neutral-500 focus:text-neutral-400 focus:outline-neutral-600"
       type="text"
       id="input"
       name="input"
