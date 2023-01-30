12e62637db7a6e3476bbd24ac9fd18c5f968fe43
Ygor Goulart <ygorgm95@gmail.com>
2023-01-29 23:11:20 -0300

    add style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 export const Footer = () => {
   return (
-    <div className="footer-container">
-      Made by <a href="https://linktr.ee/yggm">Ygor Goulart</a>
+    <div className="flex justify-center w-full">
+      Made by{" "}
+      <a
+        className="ml-2 font-bold underline decoration-neutral-700 hover:no-underline duration-200 border border-transparent rounded-lg pb-1 hover:px-3 hover:border-cyan-500  outline-cyan-500 hover:text-cyan-500 hover:shadow-cyan"
+        href="https://linktr.ee/yggm"
+      >
+        Ygor Goulart
+      </a>
     </div>
   );
 };
