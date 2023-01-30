7c98dfca3ca4c0572e1003b74c80a61be2e8b4bc
Ygor Goulart <ygorgm95@gmail.com>
2023-01-29 23:24:41 -0300

    style with tailwid

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -3,5 +3,5 @@ import { useInput } from "../contexts/InputContext";
 
 export const Output = () => {
   const { output } = useInput();
-  return <div className="output-container">{output}</div>;
+  return <div className="flex items-center justify-center flex-wrap p-2 outline outline-1 outline-neutral-800 rounded-lg">{output}</div>;
 };
