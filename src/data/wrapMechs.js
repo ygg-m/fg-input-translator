3056ae002e3ab493b6f94cd155b909b6c77bf5c4
Ygor Goulart <ygorgm95@gmail.com>
2023-01-30 22:37:20 -0300

    fix some inputs descriptions and input notations

--- src/data/wrapMechs.js
+++ src/data/wrapMechs.js
@@ -1,7 +1,16 @@
 const wrapMechs = [
   {
+    name: "",
+    input: "[X > 236X]",
+    type: "mech-complex",
+    regex: /\[(.*?)\]/g,
+    description:
+      "Group of inputs. Usually people use this to make a 'or' statement, where you can you one combo or another and still achieve the same ending.",
+    img: "",
+  },
+  {
     name: "Optional",
-    input: "(X) or (X > 236X)",
+    input: ["(X)", "(X > 236X)"],
     type: "mech-complex",
     regex: /\((?!\d{1}\))(.*?)\)/g,
     description: "Move or sequence is optional.",
@@ -9,7 +18,7 @@ const wrapMechs = [
   },
   {
     name: "Repeat",
-    input: "[X]xN or [X > 236X]xN",
+    input: ["[X]xN", "[X > 236X]xN"],
     type: "mech-complex",
     regex: /\[(?!.*\[)([^\]]+)\]x(\d{1})/g,
     description: "Repeat move or sequence N amount of times.",
