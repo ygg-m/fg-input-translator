747b9cc216cacfafd3a99bf58d39fbecc72b82f5
Ygor Goulart <ygorgm95@gmail.com>
2023-02-20 11:37:07 -0300

    style: add flex-wrap to combo

--- src/components/Combo.jsx
+++ src/components/Combo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Combo = ({ children }) => {
   return (
-    <div className="flex w-fit items-center justify-center gap-1 rounded-lg bg-neutral-800 p-2">
+    <div className="flex w-fit flex-wrap items-center justify-center gap-1 rounded-lg bg-neutral-800 p-2">
       {children}
     </div>
   );
