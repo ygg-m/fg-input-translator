6e05db171c7502bfa5401b8760992bd0f457806e
Ygor Goulart <ygorgm95@gmail.com>
2023-02-21 17:49:50 -0300

    style: add bg color to better view at screenshot

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -6,7 +6,7 @@ export const Output = () => {
   if (output.length > 0)
     return (
       <div
-        className="flex w-fit flex-wrap items-center justify-center gap-y-2 rounded-lg p-2 outline outline-1 outline-neutral-700"
+        className="flex w-fit flex-wrap items-center justify-center gap-y-2 rounded-lg bg-neutral-900 p-2 outline outline-1 outline-neutral-700"
         ref={outputRef}
       >
         {output}
