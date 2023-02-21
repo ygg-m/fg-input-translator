6affbd43cd355e92b62eff6826e6d5227e83cf16
Ygor Goulart <ygorgm95@gmail.com>
2023-02-21 17:40:08 -0300

    style: export download & image icons

--- src/images/ui/index.js
+++ src/images/ui/index.js
@@ -1,4 +1,6 @@
 export { ReactComponent as CheckIcon } from "./check.svg";
 export { ReactComponent as ChevronIcon } from "./chevron-down.svg";
+export { ReactComponent as DownloadIcon } from "./download.svg";
+export { ReactComponent as ImageIcon } from "./image.svg";
 export { ReactComponent as ShareIcon } from "./share.svg";
 export { ReactComponent as XMarkIcon } from "./xmark.svg";
