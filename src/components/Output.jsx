ec5ddc2def113f28ab235b21823b3b4ca0fc7357
Ygor Goulart <ygorgm95@gmail.com>
2023-02-21 16:59:10 -0300

    feat: add ref from useInput

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,11 +1,14 @@
 import { useInput } from "../contexts/InputContext";
 
 export const Output = () => {
-  const { output } = useInput();
+  const { output, outputRef } = useInput();
 
   if (output.length > 0)
     return (
-      <div className="flex flex-wrap items-center justify-center gap-y-2 rounded-lg p-2 outline outline-1 outline-neutral-700">
+      <div
+        className="flex w-fit flex-wrap items-center justify-center gap-y-2 rounded-lg p-2 outline outline-1 outline-neutral-700"
+        ref={outputRef}
+      >
         {output}
       </div>
     );
