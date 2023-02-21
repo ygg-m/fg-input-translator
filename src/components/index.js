d5860c7009408bb4d2d782a3e086092f5b562445
Ygor Goulart <ygorgm95@gmail.com>
2023-02-21 17:40:43 -0300

    refactor: change GameSelector to Options

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,11 @@ import "react-tooltip/dist/react-tooltip.css";
 
 export { Combo } from "./Combo.jsx";
 export { Footer } from "./Footer.jsx";
-export { GameSelect } from "./GameSelect";
 export { Header } from "./Header";
 export { Input } from "./Input";
 export { InputField } from "./InputField.jsx";
 export { Modal } from "./Modal.jsx";
+export { Options } from "./Options";
 export { Output } from "./Output.jsx";
 export { SubTechInput } from "./SubTechInput";
 export { TooltipContent } from "./TooltipContent.jsx";
