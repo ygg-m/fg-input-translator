7a4ce106a1d45fbe3857eb34ee10dd84ce1c9db6
Ygor Goulart <ygorgm95@gmail.com>
2023-02-22 11:54:11 -0300

    style: fix background and text color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="flex w-full justify-center py-8">
+    <div className="flex w-full justify-center bg-neutral-900 py-8 text-neutral-500">
       Made by{" "}
       <a
         className="hover:shadow-cyan ml-2 rounded-lg border border-transparent pb-1 font-bold underline decoration-neutral-700 outline-cyan-500 duration-200 hover:border-cyan-500  hover:px-3 hover:text-cyan-500 hover:no-underline"
